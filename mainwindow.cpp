e6bf2b1626860acd3ef4d73130878c14bfbe6098
Linus Ahlenius <Linus Ahlenius>
2015-11-05 23:49:44 +0100

    basic ui

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -12,3 +12,14 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::on_storeButton_clicked(){
+    if((ui->nameAnalyser->text() != "") && (ui->targetAnalysis->text() != "")
+            && (ui->stopLossAnalysis->text() != "") && (ui->marketBox->currentIndex() != 0)
+            && (ui->typeAnalysisBox->currentIndex() != 0) && (ui->stockBox->currentIndex() != 0)){
+        ui->labelDate->setText("Success!");
+    }
+    else{
+        ui->labelDate->setText("Fail!");
+    }
+}
